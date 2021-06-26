bc5c2e3ae416b57daa3cf9e22a88f1c615ac540f
aj-lav <ajeesh77krish@gmail.com>
2021-06-26 16:13:23 +0530

    adds changes

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-delete from books_subjects where id in (select s.id from books_subjects bs, subjects s where s.name= 'History' and s.id=bs.subject;);
+delete from books_subjects where subject in (select s.id from books_subjects bs, subjects s where s.name= 'History' and s.id=bs.subject);
 delete from subjects where name = 'History';
